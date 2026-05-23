6d3207c5973271e7c3e70d15e47b0a577d1907db
fre-denni <fredenni.git@gmail.com>
2026-05-23 16:25:32 +0200

    Added methodology to show image in local

--- src/lib/components/atoms/Image.svelte
+++ src/lib/components/atoms/Image.svelte
@@ -1,7 +1,14 @@
 <script>
+	import { dev } from '$app/environment';
 	let { src, alt, width = 828, class: className = '' } = $props();
 	let optimizedSrc = $derived(
-		src ? `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=75` : ''
+		src
+			? dev
+				? // In locale usiamo l'immagine originale
+					src
+				: // In produzione usiamo l'ottimizzatore di Vercel
+					`/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=75`
+			: ''
 	);
 </script>
 
