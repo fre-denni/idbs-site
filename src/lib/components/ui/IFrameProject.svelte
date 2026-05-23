975ca9f2eacee9ea25d256c3f2664152e8b54c5f
fre-denni <fredenni.git@gmail.com>
2026-05-23 21:21:57 +0200

    basic component to iframe

---
+++ src/lib/components/ui/IFrameProject.svelte
@@ -0,0 +1,15 @@
+<script>
+	let { vimeo = false, youtube = false, pdf = false } = $props();
+</script>
+
+<div class="container">
+	{#if vimeo}
+		<iframe src="" frameborder="0"></iframe>
+	{:else if youtube}
+		<iframe src="" frameborder="0"></iframe>
+	{:else if pdf}
+		<iframe src="" frameborder="0"></iframe>
+	{/if}
+</div>
+
+<style></style>
